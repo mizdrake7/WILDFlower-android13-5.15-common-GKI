aa37d2210cc38846e69beeebe86406c56eb545cd
yipeng xiang <yipengxiang@honor.corp-partner.google.com>
2025-02-14 11:46:58 +0800

    ANDROID: mm: create vendor hooks for mm lru_lazyfree
    Add vendor hook to add lazyfree to lru tail in lru_lazyfree
    
    Bug: 396330858
    Bug: 414563677
    Change-Id: I3d421992811fa87cf1c5c45cfe2a08e06004da83
    Signed-off-by: yipeng xiang <yipengxiang@honor.corp-partner.google.com>

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -388,7 +388,9 @@ DECLARE_HOOK(android_vh_lruvec_del_folio,
 	TP_PROTO(struct lruvec *lruvec, struct folio *folio, enum lru_list lru,
 		bool *skip),
 	TP_ARGS(lruvec, folio, lru, skip));
-
+DECLARE_HOOK(android_vh_add_lazyfree_bypass,
+	TP_PROTO(struct lruvec *lruvec, struct folio *folio, bool *bypass),
+	TP_ARGS(lruvec, folio, bypass));
 DECLARE_HOOK(android_vh_cma_debug_show_areas,
 	TP_PROTO(bool *show),
 	TP_ARGS(show));

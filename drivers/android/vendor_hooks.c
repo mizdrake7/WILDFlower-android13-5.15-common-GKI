23aa278a7e952bc5d8d0ce7e9293186ddffe4189
yangyongpeng <yangyongpeng1@oppo.com>
2025-03-25 15:54:02 +0800

    ANDROID: vendor_hook: add hooks for I/O priority
    1. Check and statistic I/O priorities
    2. Ensure critical I/O operations are not left unassigned high priority
    3. Avoid assigning high priority to excessive I/O operations to prevent
    resource contention
    
    Bug: 406175090
    Bug: 407935798
    
    Change-Id: I54b3112ac8e96b8a0fabccff85521bf05a2a585c
    Signed-off-by: yangyongpeng <yangyongpeng1@oppo.com>
    (cherry picked from commit 52e82fb490db327de31439e47cf58f7bc9a1bb5e)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -601,3 +601,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_wq_wake_idle_worker);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_dma_buf_release);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_filemap_map_pages_range);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_proactive_compact_wmark_high);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_set_ioprio);

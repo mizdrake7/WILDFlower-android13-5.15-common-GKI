BayLibre/android_kernel_common

ANDROID: disable KABI macros for VDSO and EFI libstub
If BUILD_VDSO or __DISABLED_EXPORTS are defined, disable the KABI
macros to prevent VDSO and EFI libstub link errors.

Bug: 400972353
Change-Id: I8031509bc5b481f0350735ecdf6c79f6d885daeb
Suggested-by: Sami Tolvanen <samitolvanen@google.com>
Signed-off-by: Giuliano Procida <gprocida@google.com>
Signed-off-by: John Stultz <jstultz@google.com>

include/linux/android_kabi.h
@@ -41,12 +41,15 @@
 /*
  * Worker macros, don't use these, use the ones without a leading '_'
  */
-
+#if defined(BUILD_VDSO) || defined(__DISABLE_EXPORTS)
+#define __ANDROID_KABI_RULE(hint, target, value)
+#else
 #define __ANDROID_KABI_RULE(hint, target, value)			 \
 	static const char CONCATENATE(__gendwarfksyms_rule_,		 \
 				      __COUNTER__)[] __used __aligned(1) \
 		__section(".discard.gendwarfksyms.kabi_rules") =	 \
 			"1\0" #hint "\0" target "\0" value
+#endif
 
 #define _ANDROID_KABI_RULE(hint, target, value) \
 	__ANDROID_KABI_RULE(hint, #target, #value)
@@ -143,18 +146,26 @@
  * ANDROID_KABI_IGNORE
  *   Add a new field that's ignored in versioning.
  */
+#ifndef BUILD_VDSO
 #define ANDROID_KABI_IGNORE(n, _new)		 \
 	union {					 \
 		_new;				 \
 		unsigned char __kabi_ignored##n; \
 	}
+#else
+#define ANDROID_KABI_IGNORE(n, _new)	_new
+#endif
 
 /*
  * ANDROID_KABI_REPLACE
  *   Replace a field with a compatible new field.
  */
+#ifndef BUILD_VDSO
 #define ANDROID_KABI_REPLACE(_oldtype, _oldname, _new) \
 	_ANDROID_KABI_REPLACE(_oldtype __kabi_renamed##_oldname, struct { _new; })
+#else
+#define ANDROID_KABI_REPLACE(_oldtype, _oldname, _new)	_new
+#endif
 
 /*
  * ANDROID_KABI_USE(number, _new)

mizdrake7/WILDFlower-android13-5.15-common-GKI

If BUILD_VDSO or __DISABLED_EXPORTS are defined, mostly disable the
KABI macros to prevent VDSO and EFI libstub link errors.
Bug: 427638992
(cherry picked from commit 31e7de74001ff54e104169fe4eac13532860864f)

include/linux/android_kabi.h
@@ -42,11 +42,15 @@
  * Worker macros, don't use these, use the ones without a leading '_'
  */
 
+#if defined(BUILD_VDSO) || defined(__DISABLE_EXPORTS)
+#define __ANDROID_KABI_RULE(hint, target, value)
+#else
 #define __ANDROID_KABI_RULE(hint, target, value)			 \
 	static const char CONCATENATE(__gendwarfksyms_rule_,		 \
 				      __COUNTER__)[] __used __aligned(1) \
 		__section(".discard.gendwarfksyms.kabi_rules") =	 \
 			"1\0" #hint "\0" target "\0" value
+#endif
 
 #define _ANDROID_KABI_RULE(hint, target, value) \
 	__ANDROID_KABI_RULE(hint, #target, #value)
@@ -66,6 +70,9 @@
 				__stringify(_new));			\
 	}
 
+#if defined(BUILD_VDSO) || defined(__DISABLE_EXPORTS)
+#define _ANDROID_KABI_REPLACE(_orig, _new)	_new
+#else
 #define _ANDROID_KABI_REPLACE(_orig, _new)		      \
 	union {						      \
 		_new;					      \
@@ -74,6 +81,7 @@
 		};					      \
 		_ANDROID_KABI_NORMAL_SIZE_ALIGN(_orig, _new); \
 	}
+#endif
 
 
 /*
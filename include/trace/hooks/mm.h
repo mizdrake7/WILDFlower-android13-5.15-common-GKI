bb9e0e718ac4a2259091c50ee0ba8c4978121bfd
davidwang <davidwang@realtek.com>
2025-07-19 19:31:04 +0800

    ANDROID: vendor_hooks: Add hooks in pagecache_get_page
    Add hooks for page allocation by fgp_flags and gfp_mask type.
    
    Bug: 433137955
    Change-Id: I1c70fb6ad6a83a2b688898d186d9bed97ccfe563
    Signed-off-by: davidwang <davidwang@realtek.com>

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -34,6 +34,9 @@ DECLARE_HOOK(android_vh_pagecache_get_page,
 	TP_PROTO(struct address_space *mapping, pgoff_t index,
 		int fgp_flags, gfp_t gfp_mask, struct page *page),
 	TP_ARGS(mapping, index, fgp_flags, gfp_mask, page));
+DECLARE_HOOK(android_vh_pagecache_get_page_gfp,
+	TP_PROTO(struct address_space *mapping, int fgp_flags, gfp_t *gfp_mask),
+	TP_ARGS(mapping, fgp_flags, gfp_mask));
 DECLARE_HOOK(android_vh_cma_alloc_start,
 	TP_PROTO(s64 *ts),
 	TP_ARGS(ts));
